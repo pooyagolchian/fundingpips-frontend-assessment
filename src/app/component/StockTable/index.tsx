539833d7678b2966f803dca7781b6d7c29e25be0
Pooya Golchian <pooya.golchian@gmail.com>
2025-01-15 10:06:37 +0400

    ✨💻 Refactor StockTable button styling
    ℹ️ Subsystem: com.intellij.openapi.diff.impl.patch.CharsetEP

--- src/app/component/StockTable/index.tsx
+++ src/app/component/StockTable/index.tsx
@@ -160,7 +160,7 @@ const StockTable = () => {
 							type="button"
 							key={symbol}
 							onClick={() => handleAddStock(symbol)}
-							className="bg-gray-100 text-gray-700 px-4 py-2 rounded shadow hover:bg-gray-300 transition focus:outline-none focus:ring-2 focus:ring-blue-500"
+							className="font-bold bg-gradient-to-r from-blue-200 to-green-200 text-gray-700 px-4 py-2 rounded-xl shadow-md hover:from-blue-100 hover:to-green-100 transition focus:outline-none focus:ring-2 focus:ring-blue-300"
 						>
 							{symbol}
 						</button>
